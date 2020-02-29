e835591473b2e5c1c9022982a28e2eeb6dc50999
DCastrillon0531 <DCastrillon0531@users.noreply.github.com>
2020-02-28 19:26:34 -0500

    Se codifica ejemplo de arreglo indexado y arreglo asociativo

---
+++ index.php
@@ -0,0 +1,20 @@
+<?php
+
+echo("Clase 4 Bucles, arreglos y funciones");
+echo("<br>");
+
+
+//Arreglo (Array) Indexado en php 
+$arreglo=array("Juan","María","James","Sandra");
+echo("<br>");
+print_r($arreglo);
+echo("<br>..........................");
+
+//Arreglo (Array) Asociativo en php
+$arreglo=array('usuario1'=>"Juan",'usurio2'=>"María",'usuario3'=>"James",'usuario4'=>"Sandra");
+echo("<br>");
+print_r($arreglo);
+
+
+
+?>
