8457a4cde7fec7859d924123230855574be26c40
DCastrillon0531 <DCastrillon0531@users.noreply.github.com>
2020-02-28 20:55:35 -0500

    Se codifica bucles para arreglos

--- index.php
+++ index.php
@@ -1,20 +1,52 @@
 <?php
 
-echo("Clase 4 Bucles, arreglos y funciones");
-echo("<br>");
-
+echo("Clase 4: Bucles, Arreglos y Funciones");
+echo("<br>..................................................................................");
 
 //Arreglo (Array) Indexado en php 
 $arreglo=array("Juan","María","James","Sandra");
 echo("<br>");
 print_r($arreglo);
-echo("<br>..........................");
+echo("<br>..................................................................................");
 
 //Arreglo (Array) Asociativo en php
-$arreglo=array('usuario1'=>"Juan",'usurio2'=>"María",'usuario3'=>"James",'usuario4'=>"Sandra");
+$arreglo=array('usuario1'=>"Juan",'usuario2'=>"María",'usuario3'=>"James",'usuario4'=>"Sandra");
 echo("<br>");
 print_r($arreglo);
+echo("<br>..................................................................................");
 
+//Tamaño de Arreglo (Array)
+$tamano=count($arreglo);
+echo("<br>");
+echo("El tamaño de nuestro arreglo es: ".$tamano);
+echo("<br>..................................................................................");
 
+//Bucle For
+for($centinela=0; $centinela<=10; $centinela++){
+    echo("<br>El valor de centinela es:".$centinela."<br>");
+}
+
+//Bucle For - Para recorrer arreglos (Array´s)
+echo("<br>..................................................................................");
+$arreglo=array("Juan","María","James","Sandra");
+for($centinela=0; $centinela<count($arreglo); $centinela++){
+    echo("<br>".$arreglo[$centinela]."<br>");
+}
+
+//Bucle Foreach - Para recorrer arreglos (Array´s)
+echo("<br>..................................................................................");
+$arreglo=array('usuario1'=>"Juan",'usuario2'=>"María",'usuario3'=>"James",'usuario4'=>"Sandra");
+foreach($arreglo as $clave=>$valor){
+    echo"<br>"."La Clave es: ".$clave."<br>";
+    echo"<br>".($valor)."<br>";
+}
+
+//Bucle Foreach - Para Arreglo (Array) Indexado
+echo("<br>..................................................................................");
+$arreglo=array("Juan","María","James","Sandra");
+foreach($arreglo as $clave=>$valor){
+    echo "<br> La Clave del cajon es: ".$clave;
+    echo "<br> El valor del cajon es: ".$valor."<br>";
+}
 
 ?>
